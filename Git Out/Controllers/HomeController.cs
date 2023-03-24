9e374ab2ff78a43d418024f48c1055a248b33325
Gabrielle E. McClimans <mcclimge@mail.uc.edu>
2023-03-24 09:45:56 -0400

    initial changes

--- Git Out/Controllers/HomeController.cs
+++ Git Out/Controllers/HomeController.cs
@@ -4,29 +4,14 @@ using System.Diagnostics;
 
 namespace Git_Out.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        [HttpGet]
+        public IActionResult GetName()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Ok(new { name = "Gabby" });
         }
     }
 }
